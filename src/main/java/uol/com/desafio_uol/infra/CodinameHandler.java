45f402ca498fcb12ae1b36192f7c84c04a56444b
Luan Valente <luanbvalente@hotmail.com>
2024-07-18 14:44:03 -0300

    Fazendo a requisição do Codinome nos Links de Referencia

--- src/main/java/uol/com/desafio_uol/infra/CodinameHandler.java
+++ src/main/java/uol/com/desafio_uol/infra/CodinameHandler.java
@@ -1,9 +1,24 @@
 package uol.com.desafio_uol.infra;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import uol.com.desafio_uol.domain.GroupType;
+import uol.com.desafio_uol.service.CodinameService;
 
 @Component
 public class CodinameHandler {
 
+    @Autowired
+    private CodinameService service;
 
+    public String findCodiname(GroupType groupType) {
+        if(groupType == GroupType.AVANGERS){
+            String firstMatch = service.getAvangersCodinameList().stream().findFirst().orElseThrow();
+            this.service.getAvangersCodinameList().remove(firstMatch);
+            return firstMatch;
+        }
+        String firstMatch = service.getJusticeLeagueList().stream().findFirst().orElseThrow();
+        this.service.getJusticeLeagueList().remove(firstMatch);
+        return firstMatch;
+    }
 }
--- src/main/java/uol/com/desafio_uol/service/PlayerService.java
+++ src/main/java/uol/com/desafio_uol/service/PlayerService.java
@@ -2,8 +2,10 @@ package uol.com.desafio_uol.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uol.com.desafio_uol.domain.GroupType;
 import uol.com.desafio_uol.domain.Player;
 import uol.com.desafio_uol.domain.dtos.PlayerDto;
+import uol.com.desafio_uol.infra.CodinameHandler;
 import uol.com.desafio_uol.repositories.PlayerRepository;
 
 @Service
@@ -12,8 +14,19 @@ public class PlayerService {
     @Autowired
     PlayerRepository repository;
 
+    @Autowired
+    private CodinameHandler handler;
+
     public Player createPlayer(PlayerDto dto) {
         Player newPlayer = new Player(dto);
+        String codiname = getCodiname(dto.groupType());
+        newPlayer.setCodiname(codiname);
         return repository.save(newPlayer);
     }
+
+    private String getCodiname(GroupType groupType) {
+        return handler.findCodiname(groupType);
+    }
+
+
 }
